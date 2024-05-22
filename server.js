c018102e76860805d9f64bbfcccf62869cdcafc2
yadhukrishnasm <yadhukrishna.me@gmail.com>
2024-05-22 16:45:11 +0530

    updated server.js

--- server.js
+++ server.js
@@ -6,15 +6,32 @@ const os = require("os");
 const bcrypt = require('bcrypt');
 const mongoose = require('mongoose');
 const session = require('express-session');
-
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 // MongoDB connection
-mongoose.connect('mongodb://localhost:27017/p2pchat');
+mongoose.connect('mongodb+srv://admin:root@yadhukrishnasm.hklj5a3.mongodb.net/', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log("Connected to MongoDB");
+}).catch((err) => {
+  console.error("Failed to connect to MongoDB", err);
+});
+
+// MongoDB session store
+const store = new MongoDBStore({
+  uri: 'mongodb+srv://admin:root@yadhukrishnasm.hklj5a3.mongodb.net/',
+  collection: 'sessions'
+});
+
+store.on('error', function(error) {
+  console.log(error);
+});
 
 // User schema
 const userSchema = new mongoose.Schema({
   password: { type: String, required: true },
-  username:{ type: String, required: true, unique: true }
+  username: { type: String, required: true, unique: true }
 });
 
 userSchema.pre('save', async function(next) {
@@ -31,7 +48,7 @@ const User = mongoose.model('User', userSchema);
 
 app.use(express.static("public"));
 app.use(express.static('public', {
-  setHeaders: (res, path,) => {
+  setHeaders: (res, path) => {
     if (path.endsWith('.css')) {
       res.setHeader('Content-Type', 'text/css');
     }
@@ -42,21 +59,21 @@ app.use(session({
   secret: 'keyboard cat',
   resave: false,
   saveUninitialized: true,
+  store: store,
   cookie: { secure: false }
 }));
 
 app.use(express.json());
 
 // Register a new user
-app.get('/reg',(req,res)=>{
-  res.render("reg.ejs")
-})
+app.get('/reg', (req, res) => {
+  res.render("reg.ejs");
+});
 
 app.post('/register', async (req, res) => {
   const { username, password } = req.body;
 
   try {
-    
     const user = new User({ username, password });
     await user.save();
     console.log(user);
@@ -70,15 +87,14 @@ app.post('/register', async (req, res) => {
   }
 });
 
+app.get('/', (req, res) => {
+  res.render("login.ejs");
+});
 
-
-app.get('/',(req,res)=>{
-  res.render("login.ejs")
-})
 // Login a user
 app.post('/login', async (req, res) => {
   console.log(req.body);
-  const { password,username } = req.body;
+  const { password, username } = req.body;
 
   try {
     const user = await User.findOne({ username });
@@ -92,7 +108,7 @@ app.post('/login', async (req, res) => {
     }
     req.session.username = username;
     res.send('Login successful');
-    } catch (error) {
+  } catch (error) {
     res.status(500).send('Login failed');
   }
 });
@@ -107,8 +123,8 @@ app.get("/chat", function (req, res) {
 
 const server = http.createServer(app);
 
-server.listen(process.env.PORT || 3000, () => {
-  console.log('Server is listening on port 3000');
+server.listen(process.env.PORT || 4000, () => {
+  console.log('Server is listening on port 4000');
 });
 
 const io = socketIO(server);
@@ -120,7 +136,7 @@ io.sockets.on("connection", function (socket) {
     socket.emit("log", array);
   }
 
-  //Defining Socket Connections
+  // Defining Socket Connections
   socket.on("message", function (message, room) {
     log("Client said: ", message);
     // for a real app, would be room-only (not broadcast)
@@ -141,12 +157,12 @@ io.sockets.on("connection", function (socket) {
       socket.emit("created", room, socket.id);
     } else if (numClients === 1) {
       log("Client ID " + socket.id + " joined room " + room);
-      //this message ("join") will be received only by the first client since the client has not joined the room yet
-      io.sockets.in(room).emit("join", room, clientName); //this client name is the name of the second client who wants to join
+      // this message ("join") will be received only by the first client since the client has not joined the room yet
+      io.sockets.in(room).emit("join", room, clientName); // this client name is the name of the second client who wants to join
       socket.join(room);
-      //this mesage will be received by the second client
+      // this message will be received by the second client
       socket.emit("joined", room, socket.id);
-      //this message will be received by two cleints after the join of the second client
+      // this message will be received by two clients after the join of the second client
       io.sockets.in(room).emit("ready");
     } else {
       // max two clients
@@ -154,9 +170,9 @@ io.sockets.on("connection", function (socket) {
     }
   });
 
-  socket.on("creatorname", (room,client) => {
-    // to all clients in room1 except the sender
-    socket.to(room).emit("mynameis",client);
+  socket.on("creatorname", (room, client) => {
+    // to all clients in room except the sender
+    socket.to(room).emit("mynameis", client);
   });
 
   socket.on("ipaddr", function () {
